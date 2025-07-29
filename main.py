98f524bd55c1a4d40b19603ecc322f2703c6bf09
arthwr <zenarthwr@gmail.com>
2025-07-29 12:01:50 +0500

    (chore): renames and gitignore

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from window import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(100, 100), Point(100, 300))
+    lin2 = Line(Point(100, 100), Point(300, 100))
+    win.draw_line(line, "black")
+    win.draw_line(lin2, "red")
     win.wait_for_close()
 
 
